c1d4105e7c5bafd41a987eb51d0a803101b0a0ae
gulsumerbay <glsm.erbay@gmail.com>
2021-01-16 21:00:24 +0300

    mobile icons updated.

--- js/custom.js
+++ js/custom.js
@@ -1,10 +1,37 @@
+
 jQuery(document).ready(function() {
 	
 	"use strict";
+	if( !isMobile.any() ) {
+		$('.left-nav').css({
+			position: 'absolute',
+			right: '120px'
+			}
+		);
+	}
 	// Your custom js code goes here.
 
 });
-
+	var isMobile = {
+		Android: function() {
+			return navigator.userAgent.match(/Android/i);
+		},
+			BlackBerry: function() {
+			return navigator.userAgent.match(/BlackBerry/i);
+		},
+			iOS: function() {
+			return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+		},
+			Opera: function() {
+			return navigator.userAgent.match(/Opera Mini/i);
+		},
+			Windows: function() {
+			return navigator.userAgent.match(/IEMobile/i);
+		},
+			any: function() {
+			return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+		}
+	};
 $( "a" ).click(function() {
 	$('.probootstrap-burger-menu').removeClass('active');
 		$('body').removeClass('show');
